0ff6b4e48518e79dad95f000deef6b025515fef4
Darby Smith <newman.darby2@gmail.com>
2023-01-14 16:17:43 -0500

    Remove pry

--- spec/facades/countries_facade_spec.rb
+++ spec/facades/countries_facade_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CountriesFacade do
     describe '#random_country' do
       it 'creates a country object from a randomly selected country' do
         country = CountriesFacade.random_country
-require 'pry'; binding.pry
+
         expect(country).to be_instance_of(Country)
         expect(country.name).to_not eq(nil)
       end
