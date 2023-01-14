33d933f05d369b1b2e0de8dbf6ced7f158e6b42f
Darby Smith <newman.darby2@gmail.com>
2023-01-14 12:59:05 -0500

    Add facade for recipes

---
+++ spec/facades/recipes_facades_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe RecipesFacade do
+  describe 'class methods' do
+    describe '#country_recipes' do
+      it 'creates array of recipes for that country' do
+        response = RecipesFacade.country_recipes("France")
+
+        expect(response).to be_an(Array)
+        expect(response[0]).to be_instance_of(Recipe)
+        expect(response[0].title).to_not eq(nil)
+        expect(response[0].url).to_not eq(nil)
+        expect(response[0].country).to_not eq(nil)
+        expect(response[0].image).to_not eq(nil)
+      end
+    end
+  end
+end
