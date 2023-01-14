1d6c276d494a7c3e06309c774c895f9140abf00c
Darby Smith <newman.darby2@gmail.com>
2023-01-14 16:16:43 -0500

    Add route for a random country selection

--- spec/requests/get_recipes_spec.rb
+++ spec/requests/get_recipes_spec.rb
@@ -18,5 +18,50 @@ RSpec.describe 'get recipes for a country' do
 
     expect(recipe[:attributes]).to have_key(:image)
     expect(recipe[:attributes][:image]).to be_a(String)
+
+    expect(recipe[:attributes]).to_not have_key(:uri)
+    expect(recipe[:attributes]).to_not have_key(:ingredients)
+    expect(recipe[:attributes]).to_not have_key(:calories)
+  end
+
+  it 'returns recipes for a random country if one is not selected' do
+    get '/api/v1/recipes'
+
+    expect(response).to be_successful
+
+    recipe = JSON.parse(response.body, symbolize_names: true)[:data][0]
+    expect(recipe[:attributes]).to have_key(:title)
+    expect(recipe[:attributes][:title]).to be_a(String)
+
+    expect(recipe[:attributes]).to have_key(:url)
+    expect(recipe[:attributes][:url]).to be_a(String)
+
+    expect(recipe[:attributes]).to have_key(:country)
+    expect(recipe[:attributes][:country]).to be_a(String)
+
+    expect(recipe[:attributes]).to have_key(:image)
+    expect(recipe[:attributes][:image]).to be_a(String)
+
+    expect(recipe[:attributes]).to_not have_key(:uri)
+    expect(recipe[:attributes]).to_not have_key(:ingredients)
+    expect(recipe[:attributes]).to_not have_key(:calories)
+  end
+
+  it 'returns an empty response if no country is given' do
+    get '/api/v1/recipes?country='
+
+    expect(response).to be_successful
+
+    recipe = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(recipe).to eq([])
+  end
+
+  it 'returns an empty array when an parameter is entered' do
+    get '/api/v1/recipes?country=gheryihb'
+
+    expect(response).to be_successful
+
+    recipe = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(recipe).to eq([])
   end
 end
