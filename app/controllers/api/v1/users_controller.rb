2bac5f9529bd129d39cebf2c08a0397ed412b45f
Darby Smith <newman.darby2@gmail.com>
2023-01-15 22:43:12 -0500

    WIP user post

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::UsersController < ApplicationController
+  # def create
+  #   uri = URI('localhost:3000/api/v1/users')
+  #   req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+  #   req.body = {param1: 'some value', param2: 'some other value'}.to_json
+  #   res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+  #     http.request(req)
+  #   end
+  # end
+end
