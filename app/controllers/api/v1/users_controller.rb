0713b314e6abe0aaab5f5cb1c901c55fed57f9b2
Darby Smith <newman.darby2@gmail.com>
2023-01-16 16:10:38 -0500

    Add error messages user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::UsersController < ApplicationController
     if User.new(user_params).save
       User.last.destroy
       render json: UserSerializer.new(User.create!(user_params)), status: 201
+    elsif User.find_by(email: params[:user][:email])
+      render json: { errors: "User email must be unique" }, status: 400
+    else
+      render json: { errors: "User not created" }, status: 400
     end
   end
 
--- spec/requests/post_users_spec.rb
+++ spec/requests/post_users_spec.rb
@@ -18,4 +18,44 @@ RSpec.describe 'creates a new user', type: :request do
     expect(last_user.email).to eq("athenadao@bestgirlever.com")
     expect(last_user.api_key).to_not eq(nil)
   end
+  
+  it 'returns an error if a user already exists with the email given' do
+    User.create!(name: "Athena", email: "athenadao@bestgirlever.com")
+    user = {
+      "name": "Athena Dao",
+      "email": "athenadao@bestgirlever.com"
+    }
+    
+    headers = { "CONTENT_TYPE" => "application/json" }
+    post '/api/v1/users', headers: headers, params: JSON.generate(user: user)
+
+    expect(response).to have_http_status(400)
+    expect(response.body).to eq("{\"errors\":\"User email must be unique\"}")
+  end
+
+  it 'returns an error if name is missing' do
+    user = {
+      "name": "",
+      "email": "athenadao@bestgirlever.com"
+    }
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+    post '/api/v1/users', headers: headers, params: JSON.generate(user: user)
+
+    expect(response).to have_http_status(400)
+    expect(response.body).to eq("{\"errors\":\"User not created\"}")
+  end
+
+  it 'returns an error if email is missing' do
+    user = {
+      "name": "Athena",
+      "email": ""
+    }
+
+    headers = { "CONTENT_TYPE" => "application/json" }
+    post '/api/v1/users', headers: headers, params: JSON.generate(user: user)
+
+    expect(response).to have_http_status(400)
+    expect(response.body).to eq("{\"errors\":\"User not created\"}")
+  end
 end
