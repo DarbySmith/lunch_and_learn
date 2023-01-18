b51feaa1b033262ba43f7c26144a8c760ff0de62
Darby Smith <newman.darby2@gmail.com>
2023-01-17 21:33:07 -0500

    Refactor response for invalid country in learning resource

--- spec/poros/learning_resource_spec.rb
+++ spec/poros/learning_resource_spec.rb
@@ -68,4 +68,87 @@ RSpec.describe LearningResource do
       expect(resource.country).to eq('Peru')
     end
   end
+
+  describe '#video_info' do
+    it 'returns a hash with keys if there is a video' do
+      country = "Peru"
+
+      video = {
+        id: {
+          videoId: "g9dbw1nd5"
+        },
+        snippet: {
+          title: "Video about China"
+        }
+      }
+
+      images = [
+        {
+          alt_description: "photo 1",
+          urls: {
+            regular: "url 1"
+          }
+        },
+        {
+          alt_description: "photo 2",
+          urls: {
+            regular: "url 2"
+          }
+        },
+        {
+          alt_description: "photo 3",
+          urls: {
+            regular: "url 3"
+          }
+        },
+        {
+          alt_description: "photo 4",
+          urls: {
+            regular: "url 4"
+          }
+        }
+      ]
+
+      resource = LearningResource.new(images, video, country)
+
+      expect(resource.video_info(video)).to eq({:title=>"Video about China", :youtube_video_id=>"g9dbw1nd5"})
+    end
+
+    it 'returns an empty hash if video is nil' do
+      country = "Peru"
+
+      video = nil
+
+      images = [
+        {
+          alt_description: "photo 1",
+          urls: {
+            regular: "url 1"
+          }
+        },
+        {
+          alt_description: "photo 2",
+          urls: {
+            regular: "url 2"
+          }
+        },
+        {
+          alt_description: "photo 3",
+          urls: {
+            regular: "url 3"
+          }
+        },
+        {
+          alt_description: "photo 4",
+          urls: {
+            regular: "url 4"
+          }
+        }
+      ]
+
+      resource = LearningResource.new(images, video, country)
+
+      expect(resource.video_info(video)).to eq({})
+    end
+  end
 end
